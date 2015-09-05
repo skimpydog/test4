9253a702f686c4334cf05154d18e1a6d1afab1d2
skimpydog <skimpydog@hotmail.com>
2015-09-05 18:11:25 -0500

    Update install.rb

--- beta_wordpress_permissions/recipes/install.rb
+++ beta_wordpress_permissions/recipes/install.rb
@@ -2,7 +2,9 @@ node[:deploy].each do |application, deploy|
   app_root = "#{deploy[:deploy_to]}/current"
   execute "set_permissions" do
     command "[ -d #{app_root}/wp-content/uploads ] || mkdir #{app_root}/wp-content/uploads && chown deploy:www-data #{app_root}/wp-content/uploads | chmod 777 #{app_root}/wp-content/uploads | find #{app_root}/wp-content/uploads -type d -exec chmod 777 {} \\;"
+    =begin
     #only_if { ::File.directory?("#{app_root}/wp-content/uploads") }
+    =end
     action :run
   end
 end
