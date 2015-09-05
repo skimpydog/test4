baedc7ff21065bbcaf093a3089a2a7a40098b4d9
skimpydog <skimpydog@hotmail.com>
2015-09-05 18:33:10 -0500

    Update install.rb

--- beta_wordpress_permissions/recipes/install.rb
+++ beta_wordpress_permissions/recipes/install.rb
@@ -1,1 +1,7 @@
-command "[ -d #{app_root}/wp-content/uploads ] || mkdir -p #{app_root}/wp-content/uploads && chown deploy:www-data #{app_root}/wp-content/uploads | chmod 777 #{app_root}/wp-content/uploads | find #{app_root}/wp-content/uploads -type d -exec chmod 777 {} \\;"
+node[:deploy].each do |application, deploy|
+  app_root = "#{deploy[:deploy_to]}/current"
+  execute "set_permissions" do
+    command "[ -d #{app_root}/wp-content/uploads ] || mkdir #{app_root}/wp-content/uploads && chown deploy:www-data #{app_root}/wp-content/uploads | chmod 777 #{app_root}/wp-content/uploads | find #{app_root}/wp-content/uploads -type d -exec chmod 777 {} \\;"
+    action :run
+  end
+end
