8470630af5377ce51fa2dd1573c4f6e332a4f1ed
skimpydog <skimpydog@hotmail.com>
2015-09-05 17:31:51 -0500

    Create metadata.rb

---
+++ deploy/metadata.rb
@@ -0,0 +1,6 @@
+name             "deploy"
+description      "Various deploy-related recipes"
+maintainer       "Betastream"
+maintainer_email "dev@gbeta.io"
+version          "1.0.0"
+license           "GPL2"s
