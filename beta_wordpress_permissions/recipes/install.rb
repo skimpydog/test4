6e7679d16680dfb9e81373656e6719a382490112
skimpydog <skimpydog@hotmail.com>
2015-09-05 19:31:26 -0500

    Update install.rb

--- beta_wordpress_permissions/recipes/install.rb
+++ beta_wordpress_permissions/recipes/install.rb
@@ -1,1 +1,7 @@
-node[:deploy].each do |application, dep
+node[:deploy].each do |application, deploy|
+  app_root = "#{deploy[:deploy_to]}/current"
+  execute "set_permissions" do
+    command "echo #{app_root} >> /tmp/text"
+    action :run
+  end
+end
