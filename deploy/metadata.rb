5adbf2fe5eee5fbfd1458d4a8ae114f1109bc24f
skimpydog <skimpydog@hotmail.com>
2015-09-22 13:48:39 -0500

    Update metadata.rb

--- deploy/metadata.rb
+++ deploy/metadata.rb
@@ -3,4 +3,4 @@ description      "Various deploy-related recipes"
 maintainer       "Betastream"
 maintainer_email "dev@gbeta.io"
 version          "1.0.0"
-license           "GPL2"s
+license           "GPL2"
